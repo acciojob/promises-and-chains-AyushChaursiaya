85ac190b9e191a750f0eeaf02f6b03b12b500795
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-29 21:52:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const form = document.getElementById("myForm").addEventListener('submit', function() {
+	event.preventDefault();
+	validateForm();
+});
+
+function validateForm() {
+	const age = parseInt(document.getElementById("age").value);
+	const name = document.getElementById("name").value;
+
+	if(!age || !name) {
+		alert('Please fill in all fields.');
+		return;
+	}
+
+	handleValidation(age, name);
+}
+
+
+async function handleValidation(age, name) {
+	try{
+	const massage = await new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(age >= 18) {
+				resolve(`Welcome, ${name}. You can vote.`);
+			}else{
+				reject(`Oh sorry ${name}. You aren't old enough.`);
+			}
+		}, 4000);
+	});
+	alert(massage);
+	}catch(error) {
+	alert(error);
+	}
+}
